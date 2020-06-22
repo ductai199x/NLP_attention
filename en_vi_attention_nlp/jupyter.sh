f52c6a37bff7b4452705f1ca2fd1f07ef1f03a2c
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2020-06-21 22:45:17 -0400

    add command to stop script if jupyter does not exist

--- en_vi_attention_nlp/jupyter.sh
+++ en_vi_attention_nlp/jupyter.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
-# Fires-up a Jupyter notebook by supplying a specific port
-jupyter notebook --no-browser --port=$1 > jupyter.log 2>&1 &
-echo "$!"
+command -v jupyter >/dev/null 2>&1 || { echo "I require jupyter but it's not installed.  Aborting."; exit 1; }
+
+[ $# -eq 0 ] && jupyter notebook --no-browser > jupyter.log 2>&1 &
+
+[ $# -eq 1 ] && jupyter notebook --no-browser --port=$1 > jupyter.log 2>&1 &
+
+pid=$(ps | grep jupyter | awk '{print $1}')
+sleep 2
+netstat -pant 2>/dev/null | grep $pid
+grep -o -m 1 "\(token\)=.*" jupyter.log
